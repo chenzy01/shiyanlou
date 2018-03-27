8984fd20726d36d60625383a518f068a58462068
chenzy01 <511590474@qq.com>
2018-03-27 22:57:33 +0800

    添加标签

--- read_content_from_database/news/app.py
+++ read_content_from_database/news/app.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 #-*- config:utf-8 -*-
 
-from datetime import datetiem
+from datetime import datetime
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 
 app=Flask(__name__)
-app.config.update(dict(SQLALCHEMY_DATEBASE_URI='myslq://root@localhost/shiyanlou'))
+app.config.update(dict(SQLALCHEMY_DATEBASE_URI='mysql://root@localhost/shiyanlou'))
 app.config['TEMPLATES_AUTO_RELOAD']=True
 
 db=SQLAlchemy(app)
 
-class File(self):
+mongo=MongoClient('127.0.0.1',27017).shiyanlou
+
+class File(db.Model):
     __tablename__='files'
     id=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(80),unique=True)
-    created_time=db.Column(db.DateTiem)
+    created_time=db.Column(db.DateTime)
     category_id=db.Column(db.Integer,db.ForeignKey('categories.id'))
     category=db.relationship('Category',uselist=False)
     content=db.Column(db.Text)
@@ -25,6 +28,40 @@ class File(self):
         self.created_time=created_time
         self.category=category
         self.content=content
+    
+    def add_tag(self,tag_name):
+        file_item=mongo.files.find_one({'file_id':self.id})
+        if file_item:
+            tags=file_item['tags']
+            if tag_name not in tags:
+                tags.append(tag_name)
+            mongo.files.update_one({'file_id':self.id},{'$set':{'tags':tags}})
+        else:
+            tags=[tag_name]
+            mongo.files.insert_one({'file_id':self.id,'tags':tags})
+        return tags
+
+    def remove_tag(self,tag_name):
+        file_item=mongo.files.find_one({'file_id':slef.id})
+        if file_item:
+            tags=file_item['tags']
+            try:
+                tags.remove(tag_name)
+                new_tags=tags
+            except ValueError:
+                return tags
+            mongo.files.update_one({'file_id':self.id},{'$set':{'tags':new_tags}})
+            return new_tags
+        return []
+
+    @property
+    def tags(self):
+        file_item=mongo.files.find_one({'file_id':self.id})
+        if file_item:
+            print(file_item)
+            return file_item['tags']
+        else:
+            return []
 
 class Category(db.Model):
     __tablename__='categories'
@@ -35,7 +72,7 @@ class Category(db.Model):
     def __init__(self,name):
         self.name=name
 
-def inser_datas():
+def insert_datas():
     java=Category('Java')
     python=Category('Python')
     file1=File('Hello Java',datetime.utcnow(),java,'File Content-Java is cool!')
@@ -44,7 +81,12 @@ def inser_datas():
     db.session.add(python)
     db.session.add(file1)
     db.session.add(file2)
-    db.session.commit()                
+    db.session.commit()
+    file1.add_tag('tech')
+    file1.add_tag('java')
+    file1.add_tag('linux')
+    file2.add_tag('tech')
+    file2.add_tag('python')
                          
 @app.route('/')
 def index():
@@ -59,7 +101,10 @@ def file(file_id):
 def not_found(error):
     return render_template('404.html'),404
 
-if __name__="__main__":
+if __name__=="__main__":
+    db.create_all()
+    if not Category.query.filter_by(name='Java').first():
+        insert_datas()
     app.run()
                          
                          
