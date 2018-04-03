b61358d1866828657dd40a5ba91ff54c471ace8b
chenzy01 <511590474@qq.com>
2018-04-03 22:43:38 +0800

    增加爬取User

--- Scrapy/shiyanlou/shiyanlou/models.py
+++ Scrapy/shiyanlou/shiyanlou/models.py
@@ -4,6 +4,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column,String,Integer
+from sqlalchemy import Date
+
 #初始化数据库链接
 engine=create_engine('mysql+mysqldb://root@localhost:3306/shiyanlou?charset=utf8')
 #创建对象的基类
@@ -18,6 +20,19 @@ class Course(Base):
     type=Column(String(64),index=True)
     students=Column(Integer)
 
+#创建User表
+class User(Base):
+    __tablename__='users'
+    
+    id=Column(Integer,primary_key=True)
+    name=Column(String(64),index=True)
+    type=Column(String(64))
+    status=Column(String(64),index=True)
+    school=Column(String(64))
+    job=Column(String(64))
+    level=Column(Integer,index=True)
+    join_date=Column(Date)
+    learn_courses_num=Column(Integer)
 
 if __name__=='__main__':
 #利用基类创建表    
