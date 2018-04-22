c2610bf7198caee69d91de92df645a7d4ea05187
chenzy <511590474@qq.com>
2018-04-22 10:12:16 +0800

    Update shiyanlou_github.py

--- Scrapy/shiyanlougithub/shiyanlougithub/spiders/shiyanlou_github.py
+++ Scrapy/shiyanlougithub/shiyanlougithub/spiders/shiyanlou_github.py
@@ -14,7 +14,7 @@ class ShiyanlouGithubSpider(scrapy.Spider):
     def parse(self, response):
         for repository in response.css('li.public'):
             item=ShiyanlougithubItem()
-            item['name']=repository.xpath('.//a[@itemprop="name codeRrpository"]/text()').re_first("\n\s(.*)")
+            item['name']=repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first("\n\s(.*)")
             item['update_time']=repository.xpath('.//relative-time/@datetime').extract_first()
             repo_url=response.urljoin(repository.xpath('.//a/@href').extract_first())
             request=scrapy.Request(repo_url,callback=self.parse_repo)
