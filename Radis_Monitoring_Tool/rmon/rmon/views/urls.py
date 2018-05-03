6948eb8b421f013c7aa617133bc9d62f8bf3a957
chenzy <511590474@qq.com>
2018-05-03 13:56:03 +0800

    Update urls.py

--- Radis_Monitoring_Tool/rmon/rmon/views/urls.py
+++ Radis_Monitoring_Tool/rmon/rmon/views/urls.py
@@ -1,7 +1,8 @@
+#定义所有API对应的URL
 
 from flask import Blueprint
 from rmon.views.index import IndexView
 
 api=Blueprint('api',__name__)
-
+#定义路由规则
 api.add_url_rule('/',view_func=IndexView.as_view('index'))
