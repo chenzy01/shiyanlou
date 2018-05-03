c9d7329658fc5a4a5d9ea8cb51d7602fa0a2416b
chenzy <511590474@qq.com>
2018-05-03 13:53:01 +0800

    Update index.py

--- Radis_Monitoring_Tool/rmon/rmon/views/index.py
+++ Radis_Monitoring_Tool/rmon/rmon/views/index.py
@@ -1,9 +1,10 @@
-
+#整个应用首页的控制器代码
 
 from flask import render_template
 from flask.views import MethodView
 
 class IndexView(MethodView):
+#首页视图
     def get(self):
-
+#渲染模板
         return render_template('index.html')
