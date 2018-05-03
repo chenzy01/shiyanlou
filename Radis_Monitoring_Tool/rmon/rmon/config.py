d51b5836c4598c2cd57da983a5a2d402a695c2b2
chenzy <511590474@qq.com>
2018-05-03 13:39:17 +0800

    Update config.py

--- Radis_Monitoring_Tool/rmon/rmon/config.py
+++ Radis_Monitoring_Tool/rmon/rmon/config.py
@@ -1,16 +1,18 @@
 
-
+"""
+rmon.config 配置文件
+"""
 import os
  
 class DevConfig:
-
+#开发环境配置
     DEBUG=True
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     SQLALCHEMY_DATABASE_URI='sqlite://'
     TEMPLATES_AUTO_RELOAD=True
 
 class ProductConfig(DevConfig):
+ #生产环境配置
     DEBUG=False
-
     path=os.path.join(os.getcwd(),'rmon.db').replace('\\','/')
     SQLALCHEMY_DATABASE_URI='sqlite:///%s' % path
