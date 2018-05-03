4e802c1795ea63b64b396b9589ed5be051cd73bd
chenzy <511590474@qq.com>
2018-05-03 11:41:12 +0800

    Update app.py

--- Radis_Monitoring_Tool/rmon/app.py
+++ Radis_Monitoring_Tool/rmon/app.py
@@ -1,3 +1,7 @@
+"""
+app.py,应用程序入口文件
+"""
+
 import urllib
 from rmon.app import create_app
 from rmon.models import db
@@ -6,6 +10,6 @@ app=create_app()
 
 @app.cli.command()
 def init_db():
-#
+#初始化数据库
     print("sqlite3 database file is %s" % app.fonfig['SQLALCHEMY_DATABASE_URI'])
     db.create_all()
