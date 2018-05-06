762cae3505e1a11aed4e0ca33b2f814ca9b082f9
chenzy <511590474@qq.com>
2018-05-06 21:05:49 +0800

    Update test_models.py

--- Radis_Monitoring_Tool/rmon/tests/test_models.py
+++ Radis_Monitoring_Tool/rmon/tests/test_models.py
@@ -1,13 +1,13 @@
-
+#测试用例
 from rmon.models import Server
 from rmon.common.rest import RestException
 
 
 class TestServer:
-
+#测试Server相关功能
 
     def test_save(self,db):
-
+#测试Server.save 保存服务器方法
         assert Server.query.count()==0
         server=Server(name='test',host='127.0.0.1')
         server.save()
@@ -15,9 +15,11 @@ class TestServer:
         assert Server.query.first()==server
 
     def test_delete(self,db,server):
-
+#测试Server.delete 删除服务器方法
         assert Server.query.count()==1
         server.delete()
         assert Server.query.count()==0
+        
+    
 
 
