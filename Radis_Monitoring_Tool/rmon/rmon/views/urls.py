fdb00fe84550c53f3528ac82fe19ef90cbfb2703
chenzy <511590474@qq.com>
2018-05-24 17:47:14 +0800

    Update urls.py

--- Radis_Monitoring_Tool/rmon/rmon/views/urls.py
+++ Radis_Monitoring_Tool/rmon/rmon/views/urls.py
@@ -2,8 +2,7 @@
 
 from flask import Blueprint
 from rmon.views.index import IndexView
-from rmon.views.server import ServerList
-from rmon.views.server import ServerDetail
+from rmon.views.server import （ServerList，ServerDetail，ServerCommand,ServerMetrics）
 
 
 api=Blueprint('api',__name__)
@@ -12,6 +11,11 @@ api.add_url_rule('/',view_func=IndexView.as_view('index'))
 
 api.add_url_rule('/server/',view_func=ServerList.as_view('server_list'))
 
-api.add_url_rule('/server/<int:object_id>',view_func=ServerDetail.as_view('server_detail'))
+api.add_url_rule('/server/<int:object_id>',
+                 view_func=ServerDetail.as_view('server_detail'))
 
+api.add_url_rule('/server/<int:object_id>/metrics',
+                 view_fucn=ServerMetrics.as_view('server_metrics'))
 
+api.add_url_rule('/server/<int:object_id>/command',
+                 view_fucn=ServerCommand.as_view('server_command'))
