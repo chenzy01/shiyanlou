9c17c7c259f802a82dd9e30b6bd7c0b6699b37f1
chenzy <511590474@qq.com>
2018-05-24 17:26:46 +0800

    Update index.py

--- Radis_Monitoring_Tool/rmon/rmon/views/index.py
+++ Radis_Monitoring_Tool/rmon/rmon/views/index.py
@@ -4,7 +4,7 @@ from flask import render_template
 from flask.views import MethodView
 
 class IndexView(MethodView):
-#首页视图
+    #首页视图
     def get(self):
-#渲染模板
+    #渲染模板
         return render_template('index.html')
