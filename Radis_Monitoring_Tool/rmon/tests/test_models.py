5f774ff0a7c8581388085076d311855ffb51ce01
chenzy <511590474@qq.com>
2018-05-24 17:23:29 +0800

    Update test_models.py

--- Radis_Monitoring_Tool/rmon/tests/test_models.py
+++ Radis_Monitoring_Tool/rmon/tests/test_models.py
@@ -4,10 +4,10 @@ from rmon.common.rest import RestException
 
 
 class TestServer:
-#测试Server相关功能
+    #测试Server相关功能
 
     def test_save(self,db):
-#测试Server.save 保存服务器方法
+    #测试Server.save 保存服务器方法
         assert Server.query.count()==0
         server=Server(name='test',host='127.0.0.1')
         server.save()
@@ -15,13 +15,13 @@ class TestServer:
         assert Server.query.first()==server
 
     def test_delete(self,db,server):
-#测试Server.delete 删除服务器方法
+    #测试Server.delete 删除服务器方法
         assert Server.query.count()==1
         server.delete()
         assert Server.query.count()==0
         
     def test_ping_success(self,db,sever):
-#Server.ping方法执行成功，需保证Redis服务器监听在 127.0.0.1:6379地址
+    #Server.ping方法执行成功，需保证Redis服务器监听在 127.0.0.1:6379地址
         assert server.ping() is True
     
     def test_ping_failed(sefl,db):
