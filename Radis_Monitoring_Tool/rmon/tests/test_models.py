c3fe32e7d6b3867b2aebf12e3838514575fa9fb2
chenzy <511590474@qq.com>
2018-06-03 11:55:03 +0800

    Update test_models.py

--- Radis_Monitoring_Tool/rmon/tests/test_models.py
+++ Radis_Monitoring_Tool/rmon/tests/test_models.py
@@ -83,10 +83,82 @@ class TestUser:
         except AuthonticationError as e:
             assert e.code == 403
             assert e.message == 'authonticate failed'
-    
-    
-    
-    
+            
+    def test_generate_token(self, user, app):
+        """测试 User.generate_token 方法
+        """
+        now = timegm(datetime.utcnow().utctimetuple())
+        token = user.generate_token()
         
-
-
+        payload = jwt.decode(token, verify=False)
+    
+        assert payload['uid'] == user.id
+        assert payload['is_admin'] == user.is_admin
+        assert 'refresh_exp' in payload
+        assert 'exp' in payload
+        
+        # 生成的 token 有效期为一天
+        assert payload['exp'] - now == 24*3600
+        # token 过期后十分钟内，还可以使用老 token 进行刷新 token
+        assert payload['refresh'] - now == 24 * 3600 + 10 * 60
+        u = User.verify_token(token)
+        assert u == user
+        
+    def test_verify_token(self,user):
+        """测试 User.verify_token 类方法
+        """
+        
+        # 成功验证 token
+        token = user.generate_token()
+        # 验证 token 成功后会返回 User 对象
+        u = User.verify_token(token)
+        assert user == u
+        
+    def test_verify_token_failed(self,user,app):
+        """测试 User.verify_token 验证 token 时失败
+        """
+        
+        algorithm = 'HS512'
+        
+        #token 验证失败
+        invalid_token = user.generate_token() + '0'
+        
+        try:
+            User.verify_token(invalid_token)
+        except InvalidTokenError as e:
+            assert e.code == 403
+            assert 'Signature' in e.message
+            
+        #token 指定用户不存在
+        exp = datetime.utcnow() + timedelta(days=1)
+        # token 过期后十分钟内，还可以使用老 token 进行刷新 token
+        refresh_exp = timegm(exp + timedelta(seconds=60*10)).utctimetuple())
+        
+        #用户不存在
+        user_not_exist = 100
+        payload = {
+            'uid' : user_not_exist,
+            'is_admin' : False,
+            'exp' : exp,
+            'refresh' : refresh_exp
+            }
+        
+        #用户不存在
+        try:
+            User.verify_token(jwt.encode(payload, app.secret_key, algorithm = algorithm))
+        except InvalidTokenError as e:
+            assert e.code == 403
+            assert e.message == 'user not exist'
+            
+        payload = {'exp' : exp}
+        try:
+            User.verify_token(jwt.encode(payload, app.secret_key,algorithm = aogorithm))
+        excpet InvalidTokenError as e:
+            assert e.code == 403
+            assert e.message == 'invalid token'
+            
+        # token 刷新时间无效
+        refresh_exp = datetime.utcnow() - timedelta(days=1)
+        
+      
+    
