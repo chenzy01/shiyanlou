e17f42ab5edb20e9ab25caa24851afc3e2f2c3b2
chenzy <511590474@qq.com>
2018-07-15 11:19:50 +0800

    Update forms.py

--- Online_education_website/simpledu/simpledu/forms.py
+++ Online_education_website/simpledu/simpledu/forms.py
@@ -1,15 +1,27 @@
 from flask_wtf import FlaskForm
+
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Length, Email, EqualTo, Required
 
+from simpledu.models import db,User
+
 
 class RegisterForm(FlaskForm):
+    
     username = StringField('用户名', validators=[Required(), Length(3, 24)])
     email = StringField('邮箱', validators=[Required(), Email()])
     password = PasswordField('密码', validators=[Required(), Length(6, 24)])
     repeat_password = PasswordField('重复密码', validators=[Required(), EqualTo('password')])
     submit = SubmitField('提交')
 
+    def create_user(self):
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        db.session.add(user)
+        db.seeeion.commit()
+        return user
 
 class LoginForm(FlaskForm):
     email = StringField('邮箱', validators=[Required(), Email()])
