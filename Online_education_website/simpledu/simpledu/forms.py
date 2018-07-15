c0d2010c3dfe73e230c14f10ce5cf0a24344b10f
chenzy <511590474@qq.com>
2018-07-15 11:40:31 +0800

    Update forms.py

--- Online_education_website/simpledu/simpledu/forms.py
+++ Online_education_website/simpledu/simpledu/forms.py
@@ -22,10 +22,28 @@ class RegisterForm(FlaskForm):
         db.session.add(user)
         db.seeeion.commit()
         return user
+    
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经存在')
+            
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经存在')
 
 class LoginForm(FlaskForm):
+    
     email = StringField('邮箱', validators=[Required(), Email()])
     password = PasswordField('密码', validators=[Required(), Length(6, 24)])
     remember_me = BooleanField('记住我')
     submit = SubmitField('提交')
+    
+    def validate_email(self, field):
+        if field.data and not User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱未注册')
+            
+    def validate_password(self, field):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user and not user.check_password(field.data):
+            raise ValidationError('密码错误')
 
