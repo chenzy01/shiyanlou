c351b847cf2e02ca19a2554f8ba2be4635088486
chenzy <511590474@qq.com>
2018-07-15 12:02:38 +0800

    Update app.py

--- Online_education_website/simpledu/simpledu/app.py
+++ Online_education_website/simpledu/simpledu/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from simpledu.config import configs
 from simpledu.models import db,Course, User
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from simpledu.models import db, User
 
 
 def create_app(config):
@@ -28,3 +30,16 @@ def register_blueprints(app):
     app.register_blueprint(course)
     app.register_blueprint(admin)
     app.register_blueprint(user)
+    
+def register_extensions(app):
+    db.init_app(app)
+    Migrate(app,db)
+    
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def user_loader(id):
+        return User.query.get(id)
+
+    login_manager.login_view = 'front.login'
