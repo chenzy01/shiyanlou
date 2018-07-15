63c2a2785b2109d6907d14abfe91eb8fded9cf7b
chenzy <511590474@qq.com>
2018-07-15 11:24:13 +0800

    Update front.py

--- Online_education_website/simpledu/simpledu/front.py
+++ Online_education_website/simpledu/simpledu/front.py
@@ -1,5 +1,5 @@
 from simpledu.forms import LoginForm, RegisterForm
-
+from flask import flash
 
 @front.route('/login')
 def login():
@@ -7,7 +7,11 @@ def login():
     return render_template('login.html', form=form)
 
 
-@front.register('/register')
+@front.route('/register',methods=['GET','POST'])
 def register():
     form = RegisterForm()
+    if form.validte_on_submit():
+        form.create_user()
+        flash('注册成功，请登录！','success')
+        return redirect(url_for('.login'))
     return render_template('register.html', form=form)
