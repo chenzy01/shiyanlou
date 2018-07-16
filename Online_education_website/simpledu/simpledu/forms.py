16d667e64569cc8e21f8fe6a87bb75b03a516d60
chenzy <511590474@qq.com>
2018-07-16 22:50:50 +0800

    Update forms.py

--- Online_education_website/simpledu/simpledu/forms.py
+++ Online_education_website/simpledu/simpledu/forms.py
@@ -36,14 +36,14 @@ class RegisterForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     
-    email = StringField('邮箱', validators=[Required(), Email()])
+    username = StringField('用户名', validators=[Required(), Email()])
     password = PasswordField('密码', validators=[Required(), Length(6, 24)])
     remember_me = BooleanField('记住我')
     submit = SubmitField('提交')
     
-    def validate_email(self, field):
-        if field.data and not User.query.filter_by(email=field.data).first():
-            raise ValidationError('邮箱未注册')
+    def validate_username(self, field):
+        if field.data and not User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户未注册')
             
     def validate_password(self, field):
         user = User.query.filter_by(email=self.email.data).first()
