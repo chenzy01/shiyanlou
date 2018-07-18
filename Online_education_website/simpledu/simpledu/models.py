9360938592537a166b31977710ccc5f8d302d1d9
chenzy <511590474@qq.com>
2018-07-18 23:22:52 +0800

    Update models.py

--- Online_education_website/simpledu/simpledu/models.py
+++ Online_education_website/simpledu/simpledu/models.py
@@ -57,6 +57,28 @@ class Course(Base):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, index=True, nullable=False)
+    description = db.Column(db.String(256))
+    image_url = db.Column(db.String(256))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
     author= db.relationship('User', uselist=False)
-
+    chapters = db.relationship('Chapter')
+    
+    def __repr__(self):
+        return '<Course:{}>'.format(self.name)
+    
+    
+class Chapter(Base):
+    __tablename__ = 'chapter'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, index=True)
+    description = db.Column(db.String(256))
+    vedio_url = db.Column(db.String(256))
+    vedio_duration = db.Column(db.String(24))
+    course_id = db.Column(db.Integer, db.ForeignKey('course.di', ondelete='CASCADE'))
+    course = db.relationship('Course', uselist=False)
+    
+    def __repr__(self):
+        return '<Chapter{}>'.format(self.name)
+    
+    
