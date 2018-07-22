41be5f63dcd45af91e8cf65773337cdb0923d5a1
chenzy01 <511590474@qq.com>
2018-07-22 11:58:29 +0800

    add course detail

--- Online_education_website/simpledu/simpledu/models.py
+++ Online_education_website/simpledu/simpledu/models.py
@@ -2,8 +2,9 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask import url_for
 
-# ???????? app ?
+
 db = SQLAlchemy()
 
 
@@ -65,7 +66,10 @@ class Course(Base):
     
     def __repr__(self):
         return '<Course:{}>'.format(self.name)
-    
+
+    @property
+    def url(self):
+        return url_for('course.detail',course_id=self.id)
     
 class Chapter(Base):
     __tablename__ = 'chapter'
