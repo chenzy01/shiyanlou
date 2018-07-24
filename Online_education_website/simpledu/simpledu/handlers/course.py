3a10e543c18686bb86f46c4594a6e37c0c2900f8
chenzy <511590474@qq.com>
2018-07-24 22:08:15 +0800

    Update course.py

--- Online_education_website/simpledu/simpledu/handlers/course.py
+++ Online_education_website/simpledu/simpledu/handlers/course.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import render_template
-from simpledu.models import Course
+from simpledu.models import Course， Chapter
+from flask_login import login_requied
 
 
 course = Blueprint('course',__name__,url_prefix='/courses')
@@ -10,4 +11,9 @@ def detail(course_id):
     course = Course.query.get_or_404(course_id)
     return render_template('course/detail.html',course=course)
 
+@course.route('<int:course_id>/chapters/<int:chapter_id>')
+@login_required
+def chapter(course_id, chapter_id):
+    chapter = Chapter.query.get_or_404(chapter_id)
+    return render_template('course/chapter.html, chapter=chapter')
 
