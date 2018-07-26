f71de37ecab9289fb776caac701951a4a1ac5db5
chenzy <511590474@qq.com>
2018-07-26 23:15:44 +0800

    Update admin.py

--- Online_education_website/simpledu/simpledu/handlers/admin.py
+++ Online_education_website/simpledu/simpledu/handlers/admin.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request,current_app
 from simpledu.decorators import admin_required
+from simpledu.models import Course
 
 admin = Blueprint('admin',__name__,url_prefix='/admin')
 
@@ -9,3 +11,13 @@ admin = Blueprint('admin',__name__,url_prefix='/admin')
 def index():
     return render_template('admin/index.html')
 
+@admin.route('/courses')
+@admin_required
+def courses():
+    page = request.args.get('page', default=1, type=int)
+    pagination = Course.query.paginate(
+        page=page,
+        per_page=current_app.config['ADMIN_PER_PAGE'],
+        error_out=False
+    )
+    return render_template('admin/courses.html', pagination=pagination)
